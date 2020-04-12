22a6bfa43dc32bd23d4d8e698dac4bb20f563d40
Sil silas <silikhesilas@gmail.address>
2020-04-13 00:19:51 +0300

    impact

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,26 @@
-const covid19ImpactEstimator = (data) => data;
+const factor = (data) => {
+  let getFactor;
+  if (data.periodType === 'day') {
+    getFactor = (data.timeToElapse * 1) / 3;
+  } else if (data.periodType === 'weeks') {
+    getFactor = (data.timeToElapse * 7) / 3;
+  } else {
+    getFactor = 0;
+  }
+  return getFactor;
+};
+
+
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: {
+    currentlyInfected: data.reportedCase * 10,
+    infectionsByRequestedTime: (data.reportedCase * 10) * (2 ** factor(data))
+  },
+  suvereImpact: {
+    currentlyInfected: data.reportedCase * 50,
+    infectionsByRequestedTime: (data.reportedCase * 10) * (2 ** factor(data))
+  }
+});
 
 export default covid19ImpactEstimator;
